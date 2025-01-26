aa86314a4784139ad479f82365d1918f837c6eb8
梁建麟 <1912973865@qq.com>
2025-01-26 16:19:37 +0800

    fix hometown

--- src/views/p04_hometown.vue
+++ src/views/p04_hometown.vue
@@ -2,12 +2,12 @@
   <div class="hometown" ref="backgroundRef">
     <div>
       <fade-transition>
-        <div v-show="index === 1" :class="['text', { textAppear: !transitionalValue }]" style="top: 15vh; line-height: 2;">
+        <div v-if="index === 1" :class="['text', { textAppear: !transitionalValue }]" style="top: 15vh; line-height: 2;">
           <p><b style="margin-left: 0; font-size: 22px;">{{ userData.studentSource }}</b>是你的家乡，</p>
           <p>那是一个美丽的地方，</p>
           <p>你知道那里有什么特产吗？</p>
         </div>
-        <div style="top: 55vh; text-align: right; right: 0; opacity: 1;" v-show="index === 2" class="text fade">
+        <div style="top: 55vh; text-align: right; right: 0; opacity: 1;" v-if="index === 2" class="text fade">
           <p>有<b>{{ userData.sameStudentSource }}</b>人与你来自同样的地区，</p>
           <p>来自同一片土地，</p>
           <p>天然的亲切感和默契把你们紧紧相连，</p>
